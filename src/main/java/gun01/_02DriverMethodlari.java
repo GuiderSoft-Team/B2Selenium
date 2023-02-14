b8108fcaf4c5646b70842d3ea085b5e16f8fd5e5
atezgider <atezgider@gmail.com>
2023-02-14 17:32:31 +0100

    2023-02-14 Driver methodlari ve Locators.md

--- src/main/java/gun01/_02DriverMethodlari.java
+++ src/main/java/gun01/_02DriverMethodlari.java
@@ -113,6 +113,46 @@ public class _02DriverMethodlari {
         driver.findElement(By.cssSelector("cssSelector"));
         driver.findElement(By.xpath("xpath"));
 
+        /* driver.findElement'e ait bazi methodlar asagidadir
+         */
+
+        //input ve textarea'ya text yazar, ya da özel karakterlere basar
+        driver.findElement(By.xpath("xpath")).sendKeys("gönderilecek text");
+        driver.findElement(By.xpath("xpath")).sendKeys(Keys.ENTER);
+
+        // input ve textarea'yi temizler
+        driver.findElement(By.xpath("xpath")).clear();
+
+        // clickable olan elementlere click yapar
+        driver.findElement(By.xpath("xpath")).click();
+
+        // form da <input type="submit"> butonu varsa, submit islemi yapar
+        driver.findElement(By.xpath("xpath")).submit();
+
+        // aranan element görünür mü
+        driver.findElement(By.xpath("xpath")).isDisplayed();
+
+        // aranan element enabled mi
+        driver.findElement(By.xpath("xpath")).isEnabled();
+
+        // aranan element secili mi
+        driver.findElement(By.xpath("xpath")).isSelected();
+
+        // aranan elementin text'ini verir, texti olan elementler icin
+        // ancak input, radio, checkbox, button, textarea
+        // gibi form elementlerinin text'i yoktur value'si vardir
+        driver.findElement(By.xpath("xpath")).getText();
+
+        // aranan elementin istenilen attributu'unu verir
+        driver.findElement(By.xpath("xpath")).getAttribute("attributeName");
+
+        // aranan elementin  size'ini Dimension olarak verir
+        driver.findElement(By.xpath("xpath")).getSize();
+
+        // aranan elementin location'unu Point verir
+        driver.findElement(By.xpath("xpath")).getLocation();
+
+
         /* driver.findElements "By" aranan tüm elementleri List olarak return eder
            element bulunmazsa hata vermez, null list return eder.
          */
@@ -124,5 +164,7 @@ public class _02DriverMethodlari {
         driver.findElements(By.partialLinkText("linkin textinin bir kismi"));
         driver.findElements(By.cssSelector("cssSelector"));
         driver.findElements(By.xpath("xpath"));
+
+
     }
 }
