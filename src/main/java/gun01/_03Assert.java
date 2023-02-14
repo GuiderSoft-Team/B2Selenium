4486c2e886f00b4e52156a26e6a699e55910de2c
atezgider <atezgider@gmail.com>
2023-02-14 17:37:30 +0100

    2023-02-14 Assert güncelleme

--- src/main/java/gun01/_03Assert.java
+++ src/main/java/gun01/_03Assert.java
@@ -27,37 +27,39 @@ public class _03Assert {
     }
 
 
-    void assertler(){
+    void assertAciklamalari(){
+
+        // Assert bir textNG ya da jUnit methodudur
 
         // condition'un true olmasi beklenir
         Assert.assertTrue(true);
-        Assert.assertTrue(true, "False durumunda mesaj");
+        Assert.assertTrue(true, "Conditionun False olmasi durumunda mesaj");
 
         // condition'un false olmasi beklenir
         Assert.assertFalse(false);
-        Assert.assertFalse(false, "True durumunda mesaj");
+        Assert.assertFalse(false, "Conditionun True olmasi durumunda mesaj");
 
         // degerlerin esit olmasi beklenir
         Assert.assertEquals(1,1);
-        Assert.assertEquals(1,1,"Esit olmam durumunda mesaj");
+        Assert.assertEquals(1,1,"Degerlerin esit olmamasi durumunda mesaj");
 
         // direkt Assertion error firlatir
         Assert.fail();
 
         // degerlerin farkli olmasi beklenir
         Assert.assertNotEquals(1,2);
-        Assert.assertNotEquals(1,2, "esit olma durumunda mesaj");
+        Assert.assertNotEquals(1,2, "Degerlerin esit olmasi durumunda mesaj");
 
         // Obje'nin null olmamasi beklenir
         Assert.assertNotNull(new Object());
-        Assert.assertNotNull(new Object(), "Null olma drumunda mesaj");
+        Assert.assertNotNull(new Object(), "Objenin Null olmasi drumunda mesaj");
 
         // iki objenin ayni olmasi beklenir
         Assert.assertSame(new Object(), new Object());
-        Assert.assertSame(new Object(), new Object(), "Ayni obje olmama durumunda mesaj");
+        Assert.assertSame(new Object(), new Object(), "Farkli obje olmalari durumunda mesaj");
 
         // iki objenin ayni olmamasi beklenir
         Assert.assertNotSame(new Object(), new Object());
-        Assert.assertNotSame(new Object(), new Object(), "Ayni obje olma durumunda mesaj");
+        Assert.assertNotSame(new Object(), new Object(), "Ayni obje olmalari durumunda mesaj");
     }
 }
